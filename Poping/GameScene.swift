6c1958f1432e4d941bab23d072d2339ab12f2f2f
Medi Assumani <mediassumani49@gmail.com>
2018-09-21 14:33:56 -0700

    added setUpLabel() function

--- Poping/GameScene.swift
+++ Poping/GameScene.swift
@@ -13,17 +13,44 @@ import UIKit
 
 class GameScene: SKScene {
     
+    // NEEDED CLASS VARIABLES
+    
+    var timer = Timer()
+    var playerPoints = 0
+    var pointsLabel = SKLabelNode()
+    
+    
+    
     override func didMove(to view: SKView) {
         
     }
     
+    // this function creates a single bubble
     func makeBubble() -> SKShapeNode{
+        
+        
+        
+        
         
     }
     
+    // Function to set up the label on the screen
     func setUpPointsLabel(){
         
+        // safely unwrapping the scene node in case it is nil
+        guard let scene = scene else {return}
         
+        // making the point label pretty
+        pointsLabel.fontSize = 30
+        pointsLabel.color = UIColor.white
+        updatePoints(points: playerPoints)
+        
+        // positioning the point label
+        let xPositionOfLabel = pointsLabel.frame.size.width
+        let yPositionOfLabel = (scene.frame.size.height)
+        let playerPointsPosition = CGPoint(x: xPositionOfLabel, y: yPositionOfLabel)
+        pointsLabel.position = playerPointsPosition
+        addChild(pointsLabel)
     }
     
     func generateBubbleIn(){
@@ -35,8 +62,9 @@ class GameScene: SKScene {
         
     }
     
+    // Function to update the user points on the screen
     func updatePoints(points: Int){
-        
+        pointsLabel.text = "Score : \(points)"
     }
     
     func gameOver(status: (lost: Bool, won: Bool)){
