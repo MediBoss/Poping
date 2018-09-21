51f934c4c39c32fdcf721eb4e448fa640c7d549b
Medi Assumani <mediassumani49@gmail.com>
2018-09-21 14:18:10 -0700

    added necessary functions

--- Poping/GameScene.swift
+++ Poping/GameScene.swift
@@ -9,77 +9,55 @@
 import SpriteKit
 import GameplayKit
 import Foundation
+import UIKit
 
 class GameScene: SKScene {
     
-    let scoreLabel = SKLabelNode(fontNamed: "Helvetica")
-    
-    func makeBox() {
-        
-        // define a size for a box
-        let boxSize = CGSize(width: 40, height: 40)
-        // make a red box of size
-        let box = SKSpriteNode(texture: nil, color: .red, size: boxSize)
-        box.name = "box"
-        
-        // Get a random possition within the width of the scene
-        let x = CGFloat(randomize(number: Int(size.width - 40)) + 20)
-        
-        // position the box
-        box.position.x = x
-        box.position.y = 0
-        
-        // Add the box
-        addChild(box)
-        
-        // craete an action to move the move up teh screen
-        let moveUp = SKAction.moveTo(y: size.height, duration: 4)
-        let seq = SKAction.sequence([moveUp, .removeFromParent()])
-        box.run(seq)
-    }
-    
-    // function that returns a random int from 0 to n-1
-    func randomize(number: Int) -> Int{
-        return Int(arc4random()) % number
-    }
-    
-    
-    
     override func didMove(to view: SKView) {
         
-        addChild(scoreLabel)
-        scoreLabel.text = "Score : 0"
-        scoreLabel.fontSize = 30
-        
-        scoreLabel.position.x = 
-        
-        // make a repeating action that will call the method makeBox
-        let wait = SKAction.wait(forDuration: 1.5)
-        
-        //this will call the function below
-        let makeBox = SKAction.run{
-            self.makeBox()
-        }
-        
-        let seq = SKAction.sequence([wait, makeBox])
-        let rep = SKAction.repeatForever(seq)
-        
-        run(rep)
-
     }
     
-    // when a user touches the screen
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        var score = 0
-        if let touch = touches.first {
-            // get the location of this touch in the scene
-            let location = touch.location(in: self)
-            let node = atPoint(location)
-            if node.name == "box" {
-                node.removeFromParent()
-                score += 1
-            }
-        }
+    func makeBubble() -> SKShapeNode{
+        
     }
+    
+    func setUpPointsLabel(){
+        
+        
+    }
+    
+    func generateBubbleIn(){
+        
+        
+    }
+    
+    func moveBubbleToTop(bubble: SKShapeNode){
+        
+    }
+    
+    func updatePoints(points: Int){
+        
+    }
+    
+    func gameOver(status: (lost: Bool, won: Bool)){
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    func handleTouch(touch: UITouch){
+        
+    }
+    
+    func isGameOver() -> (lost: Bool, won: Bool){
+        
+    }
+    
     
 }
+
+
+    
+
